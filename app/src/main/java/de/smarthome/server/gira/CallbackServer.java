36e194f1fb86e33bfe4664b8ff19b9a56c1a586e
DavidOno <DavidOno@users.noreply.github.com>
2020-12-03 03:55:31 +0100

    Added Https-Server. But Gradle-build does not work.

--- app/src/main/java/de/smarthome/server/gira/CallbackServer.java
+++ app/src/main/java/de/smarthome/server/gira/CallbackServer.java
@@ -1,26 +1,45 @@
 package de.smarthome.server.gira;
+import java.util.Set;
 
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RequestMapping("/")
+@RestController
 public class CallbackServer {
-
-//    @RequestMapping("/")
-//    @RestController
-//    public class SpringServer {
-//
-//        @PostMapping(path = "value")
-//        public void receiveCallBackValue(@RequestBody String body) {
-//            System.out.println(body);
-//        }
-//
-//        @PostMapping(path = "service")
-//        public void receiveCallBackService(@RequestBody String body) {
-//            System.out.println(body);
-//        }
-//
-//
-//    }
+	
+	@PostMapping(path="value")
+	public void receiveCallBackValue(@RequestBody String body) throws JsonMappingException, JsonProcessingException {
+		System.out.println(body);
+		ObjectMapper m = new ObjectMapper();
+//		Set<DeviceImpl> products = m.readValue(body, new TypeReference<Set<DeviceImpl>>() {});
+//		products.forEach(p -> System.out.println(p));
+	}
+	
+	@PostMapping(path="error")
+	public void receiveCallBackError(@RequestBody String body) throws JsonMappingException, JsonProcessingException {
+		System.out.println(body);
+		ObjectMapper m = new ObjectMapper();
+//		Set<ServerError> products = m.readValue(body, new TypeReference<Set<ServerError>>() {});
+//		products.forEach(p -> System.out.println(p));
+	}
+	
+	@PostMapping(path="service")
+	public void receiveCallBackService(@RequestBody String body) {
+		System.out.println(body);
+		
+	}
+	
+	
+//	@PostMapping(path="json")
+//	public void receiveJson(@RequestBody DeviceImpl body) {
+//		System.out.println(body);
+//	}
 }
