8ba590361eaa8ee2c8fdd6c0b30d834770c41c14
Muriskk <skryreSlaveMail@gmail.com>
2021-03-26 18:58:13 +0100

    Adapter for Regulation RecyclerView

--- app/src/main/java/de/smarthome/server/adapter/RegulationAdapter.java
+++ app/src/main/java/de/smarthome/server/adapter/RegulationAdapter.java
@@ -1,100 +1,133 @@
 package de.smarthome.server.adapter;
 
-import android.view.LayoutInflater;
+
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import de.smarthome.R;
-import de.smarthome.model.impl.Function;
+import de.smarthome.model.configs.ChannelConfig;
+import de.smarthome.model.configs.ChannelDatapoint;
+import de.smarthome.model.impl.Datapoint;
+import de.smarthome.model.repository.Repository;
+import de.smarthome.server.adapter.viewholder.regulation.DoubleSliderViewHolder;
+import de.smarthome.server.adapter.viewholder.regulation.ReadViewHolder;
+import de.smarthome.server.adapter.viewholder.regulation.SliderViewHolder;
+import de.smarthome.server.adapter.viewholder.regulation.StepViewHolder;
+import de.smarthome.server.adapter.viewholder.regulation.SwitchViewHolder;
 
 
-public class RegulationAdapter extends RecyclerView.Adapter<RegulationAdapter.UIHolder> {
+public class RegulationAdapter extends RecyclerView.Adapter<RegulationAdapter.ViewHolder>{
+    List<Datapoint> datapointList;
 
-    private List<Function> functionList = new ArrayList<>();
+    public static final int STEP_VIEW_HOLDER = 0;
+    public static final int SWITCH_VIEW_HOLDER = 1;
+    public static final int SLIDER_VIEW_HOLDER = 2;
+    public static final int DOUBLE_SLIDER_VIEW_HOLDER = 3;
+    public static final int READ_VIEW_HOLDER = 4;
+
     private OnItemClickListener listener;
+    private OnSwitchClickListener switchClickListener;
 
-
-    @NonNull
-    @Override
-    public UIHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_switch, parent, false);
-
-        return new UIHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull UIHolder holder, int position) {
-        Function currentWorkoutPosition = functionList.get(position);
-
-        holder.textViewName.setText(currentWorkoutPosition.getName());
-
-    }
-
-    /**Returns the size of the workoutList
-     * @return current size of the workoutList
-     */
-    @Override
-    public int getItemCount() {
-        return functionList.size();
-    }
-
-
-    public void setFunctionList(List<Function> functions) {
-        functionList = functions;
+    private ChannelConfig channelConfig;
+    private Repository repository;
+    public void setDatapointList(List<Datapoint> datapoints) {
+        this.datapointList = datapoints;
+        repository = Repository.getInstance();
+        channelConfig = repository.getSmartHomeChannelConfig();
         notifyDataSetChanged();
     }
 
-    /**Returns the Workout in the workoutList at a given position
-     * @param  position position in the list
-     * @return Workout at the specified position
-     */
-    public Function getFunctionAt(int position) {
-        return functionList.get(position);
-    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-
-
-    class UIHolder extends RecyclerView.ViewHolder {
-        TextView textViewName;
-
-        /**Creates the onItemClickListeners, onItemLingClickListeners and the UI for the list items
-         */
-        public UIHolder(View itemView) {
-            super(itemView);
-
-            textViewName = itemView.findViewById(R.id.text_view_name);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int position = getAdapterPosition();
-                    if (listener != null && position != RecyclerView.NO_POSITION) {
-                        listener.onItemClick(functionList.get(position));
-                    }
-                }
-            });
+        if(viewType == SWITCH_VIEW_HOLDER){
+            return new SwitchViewHolder(
+                    parent,
+                    listener,
+                    this
+            );
+        }else if(viewType == STEP_VIEW_HOLDER){
+            return new StepViewHolder(
+                    parent,
+                    listener,
+                    this
+            );
+        }else if(viewType == SLIDER_VIEW_HOLDER){
+            return new SliderViewHolder(
+                    parent,
+                    listener,
+                    this
+            );
+        }else if(viewType == DOUBLE_SLIDER_VIEW_HOLDER){
+            return new DoubleSliderViewHolder(
+                    parent,
+                    listener,
+                    this
+            );
+        }else if(viewType == READ_VIEW_HOLDER){
+            return new ReadViewHolder(
+                    parent,
+                    listener,
+                    this
+            );
+        }else {
+            throw new IllegalArgumentException("Input type not known");
         }
     }
 
-    /**Handles short onClick Events
-     */
-    public interface OnItemClickListener {
-        void onItemClick(Function function);
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Datapoint dp = datapointList.get(position);
+        holder.onBindViewHolder(holder, position, dp);
+    }
+
+    @Override
+    public int getItemCount() {
+        return datapointList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position){
+        //TODO: This code is terrible to read. It has to be reworked.
+        return channelConfig.getRegulationItemViewType(
+                channelConfig.findChannelByName(repository.getSelectedFunction()).getDatapoints().get(position));
     }
 
 
-    /**Sets listener for short onClick Events
-     */
-    public void setOnItemClickListener(OnItemClickListener listener) {
+    public Datapoint getDatapointAt(int position) {
+        return datapointList.get(position);
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Datapoint datapoint, String value);
+    }
+
+    public void setOnItemClickListener(RegulationAdapter.OnItemClickListener listener) {
         this.listener = listener;
     }
 
+
+    public interface OnSwitchClickListener {
+        void onItemClick(Datapoint datapoint, boolean isChecked);
+    }
+
+    public void setOnSwitchClickListener(RegulationAdapter.OnSwitchClickListener listener) {
+        this.switchClickListener = listener;
+    }
+
+    public abstract static class ViewHolder extends RecyclerView.ViewHolder {//implements View.OnClickListener {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public abstract void onBindViewHolder(ViewHolder holder, int position, Datapoint datapoint);
+
+    }
 }
