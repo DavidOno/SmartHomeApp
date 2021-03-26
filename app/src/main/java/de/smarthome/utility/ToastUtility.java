849108a0e96dd8383b9236ca15d5a32c10442fe6
Muriskk <skryreSlaveMail@gmail.com>
2021-03-26 18:42:57 +0100

    Added ToastUtility to be able to toast from every class

---
+++ app/src/main/java/de/smarthome/utility/ToastUtility.java
@@ -0,0 +1,63 @@
+package de.smarthome.utility;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+/**
+ * Utility to create Toasts globally.
+ * Reason: All interactions with a database may result in a failure,
+ * but passing the necessary information to notify the user as parameter every time,
+ * makes the code less readable and cumbersome.
+ * Since it is relevant for all database-interactions,
+ * I think it's an appropriate and justified solution to make this functionality global.
+ */
+public class ToastUtility {
+
+    private static ToastUtility instance;
+    private MutableLiveData<Boolean> isToastNew = new MutableLiveData<>(false);
+    private String message;
+
+    /**
+     * Private constructor should enforce the singleton-pattern.
+     */
+    private ToastUtility() {
+        //empty constructor
+    }
+
+    /**
+     * Ensures that only a single instance is in use.
+     *
+     * @return Returns an instance of the ToastUtility.
+     */
+    public static ToastUtility getInstance() {
+        if (instance == null) {
+            instance = new ToastUtility();
+        }
+        return instance;
+    }
+
+    /**
+     * Toast is getting prepared.
+     *
+     * @param message The message to display via Toast.
+     */
+    public void prepareToast(String message) {
+        this.message = message;
+        isToastNew.postValue(true);
+    }
+
+    public LiveData<Boolean> getNewToast() {
+        return isToastNew;
+    }
+
+    /**
+     * Returns the Message.
+     *
+     * @return Returns the message, which should be displayed via Toast.
+     */
+    public String getMessage() {
+        String result = message;
+        isToastNew.postValue(false);
+        return result;
+    }
+}
