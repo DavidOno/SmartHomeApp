01e8cc477fbc65aef471eaeb825f8d593b07fab9
Muriskk <skryreSlaveMail@gmail.com>
2021-03-26 18:58:47 +0100

    Reworked how ViewHolders are chosen and created

--- app/src/main/java/de/smarthome/server/adapter/RoomOverviewAdapter.java
+++ app/src/main/java/de/smarthome/server/adapter/RoomOverviewAdapter.java
@@ -1,118 +1,121 @@
 package de.smarthome.server.adapter;
 
-import android.view.LayoutInflater;
+
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import de.smarthome.R;
+import de.smarthome.model.configs.ChannelConfig;
 import de.smarthome.model.impl.Function;
+import de.smarthome.model.repository.Repository;
+import de.smarthome.server.adapter.viewholder.roomoverview.DefaultViewHolder;
+import de.smarthome.server.adapter.viewholder.roomoverview.StatusViewHolder;
+import de.smarthome.server.adapter.viewholder.roomoverview.SwitchViewHolder;
+import de.smarthome.server.adapter.viewholder.roomoverview.SwitchArrowViewHolder;
 
 
-public class RoomOverviewAdapter extends RecyclerView.Adapter<RoomOverviewAdapter.FunctionHolder> {
+public class RoomOverviewAdapter extends RecyclerView.Adapter<RoomOverviewAdapter.ViewHolder>{
+    List<Function> functionList;
+    public static final int DEFAULT_VIEW_HOLDER = 0;
+    public static final int SWITCH_VIEW_HOLDER = 1;
+    public static final int SWITCH_ARROW_HOLDER = 2;
+    public static final int STATUS_VIEW_HOLDER = 3;
 
-    private List<Function> functionList = new ArrayList<>();
     private OnItemClickListener listener;
+    private OnSwitchClickListener switchClickListener;
 
-
-    @NonNull
-    @Override
-    public FunctionHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        View itemView = getFunctionView(parent, viewType);
-
-        return new FunctionHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull FunctionHolder holder, int position) {
-        Function currentFunctionPosition = functionList.get(position);
-
-        holder.textViewName.setText(currentFunctionPosition.getName());
-
-    }
-
-    public View getFunctionView(@NonNull ViewGroup parent, int viewType){
-        View itemView = null;
-
-        /*switch (viewType){
-            case 1:
-                itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_room_overview_switch, parent, false);
-                break;
-            case 2:
-                itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_room_overview_test, parent, false);
-                break;
-            default:
-                itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_room_overview_default, parent, false);
-                break;
-        }*/
-
-
-        return itemView;
-    }
-
-    /**Returns the size of the workoutList
-     * @return current size of the workoutList
-     */
-    @Override
-    public int getItemCount() {
-        return functionList.size();
-    }
-
+    private ChannelConfig channelConfig;
 
     public void setFunctionList(List<Function> functions) {
         functionList = functions;
         notifyDataSetChanged();
     }
 
-    /**Returns the Workout in the workoutList at a given position
-     * @param  position position in the list
-     * @return Workout at the specified position
-     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        if(viewType == SWITCH_VIEW_HOLDER){
+            return new SwitchViewHolder(
+                    parent,
+                    switchClickListener,
+                    this
+            );
+        }else if(viewType == SWITCH_ARROW_HOLDER){
+            return new SwitchArrowViewHolder(
+                    parent,
+                    listener,
+                    switchClickListener,
+                    this
+
+            );
+        }else if(viewType == STATUS_VIEW_HOLDER){
+            return new StatusViewHolder(
+                    parent,
+                    listener,
+                    this
+
+            );
+        }else{  //if(viewType == DEFAULT_VIEW_HOLDER){
+            return new DefaultViewHolder(
+                    parent,
+                    listener,
+                    this
+            );
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Function f = functionList.get(position);
+        holder.onBindViewHolder(holder, position, f);
+    }
+
+    @Override
+    public int getItemCount() {
+        return functionList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position){
+        Function item = getFunctionAt(position);
+        channelConfig = Repository.getInstance().getSmartHomeChannelConfig();
+        return channelConfig.getRoomOverviewItemViewType(channelConfig.findChannelByName(item));
+    }
+
+
     public Function getFunctionAt(int position) {
         return functionList.get(position);
     }
 
-
-
-    class FunctionHolder extends RecyclerView.ViewHolder {
-        TextView textViewName;
-
-        /**Creates the onItemClickListeners, onItemLingClickListeners and the UI for the list items
-         */
-        public FunctionHolder(View itemView) {
-            super(itemView);
-
-            textViewName = itemView.findViewById(R.id.text_view_name);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int position = getAdapterPosition();
-                    if (listener != null && position != RecyclerView.NO_POSITION) {
-                        listener.onItemClick(functionList.get(position));
-                    }
-                }
-            });
-        }
-    }
-
-    /**Handles short onClick Events
-     */
     public interface OnItemClickListener {
         void onItemClick(Function function);
     }
 
-
-    /**Sets listener for short onClick Events
-     */
-    public void setOnItemClickListener(OnItemClickListener listener) {
+    public void setOnItemClickListener(RoomOverviewAdapter.OnItemClickListener listener) {
         this.listener = listener;
+    }
+
+
+    public interface OnSwitchClickListener {
+        void onItemClick(Function function, boolean isChecked);
+    }
+
+    public void setOnSwitchClickListener(RoomOverviewAdapter.OnSwitchClickListener listener) {
+        this.switchClickListener = listener;
+    }
+
+    public abstract static class ViewHolder extends RecyclerView.ViewHolder {//implements View.OnClickListener {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public abstract void onBindViewHolder(ViewHolder holder, int position, Function function);
+
     }
 }
