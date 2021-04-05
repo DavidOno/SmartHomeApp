986cc2a93fc40f31acd0c2a201afe30c459ba781
Muriskk <skryreSlaveMail@gmail.com>
2021-04-05 11:34:19 +0200

    Added functionality for status updates

--- app/src/main/java/de/smarthome/server/adapter/RoomOverviewAdapter.java
+++ app/src/main/java/de/smarthome/server/adapter/RoomOverviewAdapter.java
@@ -1,15 +1,21 @@
 package de.smarthome.server.adapter;
 
 
+import android.app.Application;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import de.smarthome.model.configs.ChannelConfig;
+import de.smarthome.model.impl.Datapoint;
 import de.smarthome.model.impl.Function;
 import de.smarthome.model.repository.Repository;
 import de.smarthome.server.adapter.viewholder.roomoverview.DefaultViewHolder;
@@ -20,6 +26,8 @@ import de.smarthome.server.adapter.viewholder.roomoverview.SwitchArrowViewHolder
 
 public class RoomOverviewAdapter extends RecyclerView.Adapter<RoomOverviewAdapter.ViewHolder>{
     List<Function> functionList;
+    Map<Function, Function> functionMap;
+
     public static final int DEFAULT_VIEW_HOLDER = 0;
     public static final int SWITCH_VIEW_HOLDER = 1;
     public static final int SWITCH_ARROW_HOLDER = 2;
@@ -28,11 +36,43 @@ public class RoomOverviewAdapter extends RecyclerView.Adapter<RoomOverviewAdapte
     private OnItemClickListener listener;
     private OnSwitchClickListener switchClickListener;
 
+    private String statusFunctionUID = null;
+    private String statusFunctionValue = null;
+    private Function functionToBeUpdated = null;
+
     private ChannelConfig channelConfig;
 
-    public void setFunctionList(List<Function> functions) {
-        functionList = functions;
+    public void setFunctionList(Map<Function, Function> functions, Application application) {
+        functionList = new ArrayList<>(functions.keySet());
+        functionMap = functions;
+
+        channelConfig = Repository.getInstance(application).getSmartHomeChannelConfig();
+
         notifyDataSetChanged();
+    }
+
+    public void updateStatusValue(String changedStatusFunctionUID, String changedStatusFunctionValue){
+        if(containsViewStatusFunction(changedStatusFunctionUID)){
+            setStatusVariables(changedStatusFunctionUID, changedStatusFunctionValue);
+            notifyDataSetChanged();
+        }
+    }
+
+    private boolean containsViewStatusFunction(String changedStatusFunctionUID) {
+        for(Function function : functionList){
+            if (functionMap.get(function) != null) {
+                Function statusFunction = functionMap.get(function);
+
+                //TODO: Right now in RoomOverview it can only be the first dataPoint (because only binary is shown)
+                for(Datapoint datapoint : statusFunction.getDataPoints()){
+                    if (changedStatusFunctionUID.equals(datapoint.getID())) {
+                        functionToBeUpdated = function;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     @NonNull
@@ -71,8 +111,28 @@ public class RoomOverviewAdapter extends RecyclerView.Adapter<RoomOverviewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Function f = functionList.get(position);
-        holder.onBindViewHolder(holder, position, f);
+        Function function = functionList.get(position);
+
+        Optional<String> value = Optional.empty();
+        value = getStatusValueString(function, value);
+
+        holder.onBindViewHolder(holder, position, function, value);
+    }
+
+    private Optional<String> getStatusValueString(Function function, Optional<String> value) {
+        if(functionToBeUpdated != null) {
+            if(functionToBeUpdated.equals(function)){
+                value = Optional.ofNullable(statusFunctionValue);
+
+                setStatusVariables(null, null);
+            }
+        }
+        return value;
+    }
+
+    private void setStatusVariables(String uID, String value) {
+        statusFunctionUID = uID;
+        statusFunctionValue = value;
     }
 
     @Override
@@ -83,7 +143,6 @@ public class RoomOverviewAdapter extends RecyclerView.Adapter<RoomOverviewAdapte
     @Override
     public int getItemViewType(int position){
         Function item = getFunctionAt(position);
-        channelConfig = Repository.getInstance().getSmartHomeChannelConfig();
         return channelConfig.getRoomOverviewItemViewType(channelConfig.findChannelByName(item));
     }
 
@@ -115,7 +174,7 @@ public class RoomOverviewAdapter extends RecyclerView.Adapter<RoomOverviewAdapte
             super(itemView);
         }
 
-        public abstract void onBindViewHolder(ViewHolder holder, int position, Function function);
+        public abstract void onBindViewHolder(ViewHolder holder, int position, Function function, Optional<String> value);
 
     }
 }
