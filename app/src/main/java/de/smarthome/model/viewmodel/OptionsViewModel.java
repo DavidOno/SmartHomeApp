f52856ace56ae3d88d6bc40ff54cac129559978c
Muriskk <skryreSlaveMail@gmail.com>
2021-04-16 19:47:59 +0200

    Added functionality

--- app/src/main/java/de/smarthome/model/viewmodel/OptionsViewModel.java
+++ app/src/main/java/de/smarthome/model/viewmodel/OptionsViewModel.java
@@ -5,10 +5,21 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.google.android.gms.auth.api.credentials.Credential;
+
+import de.smarthome.model.repository.Repository;
+
 public class OptionsViewModel extends AndroidViewModel {
     private  final String TAG = "OptionsViewModel";
+    private Repository repository;
 
     public OptionsViewModel(@NonNull Application application) {
         super(application);
+
+        repository = Repository.getInstance(application);
+    }
+
+    public Credential getUserCredential(){
+        return repository.getUserCredential();
     }
 }
