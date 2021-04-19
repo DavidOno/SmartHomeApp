31dfc3cfc049c02c6b679c312ba07ee7b2e8f9f6
Muriskk <skryreSlaveMail@gmail.com>
2021-04-19 17:53:01 +0200

    Added Repository

--- app/src/main/java/de/smarthome/command/impl/ResponseReactorClient.java
+++ app/src/main/java/de/smarthome/command/impl/ResponseReactorClient.java
@@ -5,15 +5,20 @@ import android.util.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import de.smarthome.app.repository.Repository;
 import de.smarthome.command.ResponseReactor;
 import de.smarthome.app.utility.ToastUtility;
 
 public class ResponseReactorClient implements ResponseReactor {
     private final String TAG = "ResponseReactorClient";
+    private Repository parentRepository;
 
     private ToastUtility toastUtility;
 
-    public ResponseReactorClient() {
+    public ResponseReactorClient(Repository parentRepository) {
+        this.toastUtility = ToastUtility.getInstance();
+        this.parentRepository = parentRepository;
+
         this.toastUtility = ToastUtility.getInstance();
     }
 
@@ -25,15 +30,14 @@ public class ResponseReactorClient implements ResponseReactor {
                 System.out.println(responseEntity.getBody());
 
                 Log.d(TAG, "No Problems when registering Client at Gira.\nStatus: " + responseEntity.getStatusCode());
+                parentRepository.updateLoginDataStatus(true);
 
-                toastUtility.prepareToast("User successfully registered!");
             } else {
                 System.out.println("error occurred");
                 System.out.println(responseEntity.getStatusCode());
 
                 Log.d(TAG, "Problem when registering Client at Gira.\nStatus: " + responseEntity.getStatusCode());
-
-                toastUtility.prepareToast("Exception: Unable to register User at Gira!");
+                parentRepository.updateLoginDataStatus(false);
             }
         }catch(Exception e){
             Log.d(TAG, "Exerption: " + e.toString());
