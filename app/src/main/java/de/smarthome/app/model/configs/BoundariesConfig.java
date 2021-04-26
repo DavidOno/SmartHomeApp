cc28d23bd46f15a7042ff31d3e1cdd4fb50858ef
Muriskk <skryreSlaveMail@gmail.com>
2021-04-26 15:33:05 +0200

    Created BoundariesConfig Class

--- app/src/main/java/de/smarthome/app/model/configs/BoundariesConfig.java
+++ app/src/main/java/de/smarthome/app/model/configs/BoundariesConfig.java
@@ -1,4 +1,24 @@
 package de.smarthome.app.model.configs;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class BoundariesConfig {
+    private final List<Boundary> boundaries;
+
+    public BoundariesConfig(@JsonProperty("boundaries") List<Boundary> boundaries) {
+        this.boundaries = boundaries;
+    }
+
+    public List<Boundary> getBoundaries() {
+        return boundaries;
+    }
+
+    @Override
+    public String toString() {
+        return "BoundariesConfig{" +
+                "boundaries=" + boundaries +
+                '}';
+    }
 }
