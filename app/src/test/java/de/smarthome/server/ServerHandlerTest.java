f557a1d89deb8fe1fe9957193696f8566fdc6689
DavidOno <DavidOno@users.noreply.github.com>
2021-05-06 23:02:11 +0200

    Added dummy testcases and improved gradle output for tests

--- app/src/test/java/de/smarthome/server/ServerHandlerTest.java
+++ app/src/test/java/de/smarthome/server/ServerHandlerTest.java
@@ -1,11 +1,23 @@
 package de.smarthome.server;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ServerHandlerTest {
 
     @Test
     public void test(){
+        assertThat("abc").isEqualTo("abc");
+    }
 
+    @Test
+    public void test2(){
+        assertThat("abc").isEqualTo("abc");
+    }
+
+    @Test
+    public void test3(){
+        assertThat("abc").isEqualTo("abc");
     }
 }
