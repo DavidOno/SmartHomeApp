0c751ba72d4c44ac3bbe2ea2a68e03ddc79a0273
Muriskk <skryreSlaveMail@gmail.com>
2021-05-23 17:01:31 +0200

    Added Location for better identification

--- app/src/main/java/de/smarthome/app/model/configs/Boundary.java
+++ app/src/main/java/de/smarthome/app/model/configs/Boundary.java
@@ -6,15 +6,21 @@ import java.util.List;
 
 public class Boundary {
     private final String boundaryName;
+    private final String loaction;
     private final List<BoundaryDataPoint> datapoints;
 
-    public Boundary(@JsonProperty("boundary") String boundaryName, @JsonProperty("datapoints") List<BoundaryDataPoint> datapoints) {
+    public Boundary(@JsonProperty("boundary") String boundaryName, @JsonProperty("location") String location, @JsonProperty("datapoints") List<BoundaryDataPoint> datapoints) {
         this.boundaryName = boundaryName;
+        this.loaction = location;
         this.datapoints = datapoints;
     }
 
     public String getName() {
         return boundaryName;
+    }
+
+    public String getLoaction() {
+        return loaction;
     }
 
     public List<BoundaryDataPoint> getDatapoints() {
