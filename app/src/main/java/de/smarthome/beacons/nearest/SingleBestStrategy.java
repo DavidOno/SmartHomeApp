4a2b32ff33d73b741b094acb9784729365de7623
DavidOno <DavidOno@users.noreply.github.com>
2021-07-26 22:57:50 +0200

    Refactored Beacons

--- app/src/main/java/de/smarthome/beacons/nearest/SingleBestStrategy.java
+++ app/src/main/java/de/smarthome/beacons/nearest/SingleBestStrategy.java
@@ -7,6 +7,6 @@ import de.smarthome.beacons.BeaconID;
 public class SingleBestStrategy implements RetrievingStrategy{
     @Override
     public BeaconID getNearest(Map<BeaconID, Integer> updatedBeaconSignals) {
-        return updatedBeaconSignals.entrySet().stream().max((e1, e2) -> e1.getValue() > e2.getValue() ? 1 : -1).get().getKey();
+        return updatedBeaconSignals.entrySet().stream().min((e1, e2) -> e1.getValue() > e2.getValue() ? 1 : -1).get().getKey();
     }
 }
---
+++ app/src/test/java/de/smarthome/beacons/SingleBestTest.java
@@ -0,0 +1,45 @@
+package de.smarthome.beacons;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.smarthome.beacons.nearest.RetrievingStrategy;
+import de.smarthome.beacons.nearest.SingleBestStrategy;
+import de.smarthome.beacons.nearest.ThresholderStrategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SingleBestTest {
+    @Test
+    public void testStrongestBeaconConsistency_twoBeaconComparison() {
+        RetrievingStrategy strategy = new SingleBestStrategy();
+        Map<BeaconID, Integer> newSignals = new HashMap<>();
+        BeaconID beaconID1 = mock(BeaconID.class);
+        BeaconID beaconID2 = mock(BeaconID.class);
+        newSignals.put(beaconID1, -50);
+        newSignals.put(beaconID2, -90);
+        BeaconID nearest = strategy.getNearest(newSignals);
+
+        assertThat(nearest).isEqualTo(beaconID2);
+    }
+
+    @Test
+    public void testStrongestBeaconConsistency_threeBeaconComparison() {
+        RetrievingStrategy strategy = new SingleBestStrategy();
+        Map<BeaconID, Integer> newSignals = new HashMap<>();
+        BeaconID beaconID1 = mock(BeaconID.class);
+        BeaconID beaconID2 = mock(BeaconID.class);
+        BeaconID beaconID3 = mock(BeaconID.class);
+        newSignals.put(beaconID1, -50);
+        newSignals.put(beaconID2, -99);
+        newSignals.put(beaconID3, -90);
+        BeaconID nearest = strategy.getNearest(newSignals);
+
+        assertThat(nearest).isEqualTo(beaconID2);
+    }
+}
