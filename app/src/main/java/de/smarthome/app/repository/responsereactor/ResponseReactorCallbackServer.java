2f60a13cf71c2771d670b18d0c12dfaf1ec51296
Muriskk <skryreSlaveMail@gmail.com>
2021-08-04 16:26:13 +0200

    Added method calls to verify connection status in repository

--- app/src/main/java/de/smarthome/app/repository/responsereactor/ResponseReactorCallbackServer.java
+++ app/src/main/java/de/smarthome/app/repository/responsereactor/ResponseReactorCallbackServer.java
@@ -5,15 +5,18 @@ import android.util.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import de.smarthome.app.repository.Repository;
 import de.smarthome.command.ResponseReactor;
 import de.smarthome.app.utility.ToastUtility;
 
 public class ResponseReactorCallbackServer implements ResponseReactor {
     private static final String TAG = "ResponseReactorCallbackServer";
     private ToastUtility toastUtility;
+    private Repository repository;
 
     public ResponseReactorCallbackServer() {
-        this.toastUtility = ToastUtility.getInstance();
+        repository = Repository.getInstance();
+        toastUtility = ToastUtility.getInstance();
     }
 
     @Override
@@ -22,16 +25,18 @@ public class ResponseReactorCallbackServer implements ResponseReactor {
             if (responseEntity.getStatusCode() == HttpStatus.OK) {
                 Log.d(TAG, "response received " + TAG);
                 Log.d(TAG, "Registered CallbackServer.\nStatus: " + responseEntity.getStatusCode());
+                repository.serverConnectionEvent(ServerConnectionEvent.CALLBACK_CONNECTION_SUCCESS);
             } else {
                 Log.d(TAG, "error occurred");
                 Log.d(TAG, responseEntity.getStatusCode().toString());
-
                 Log.d(TAG, "Problem when trying to register CallbackServer.\nStatus: " + responseEntity.getStatusCode());
                 toastUtility.prepareToast("Unable to reach CallbackServer");
+                repository.serverConnectionEvent(ServerConnectionEvent.CALLBACK_CONNECTION_FAIL);
             }
         }catch(Exception e){
             Log.d(TAG, "Exception: " + e.toString());
             toastUtility.prepareToast("Unable to register CallbackServer!");
+            repository.serverConnectionEvent(ServerConnectionEvent.CALLBACK_CONNECTION_FAIL);
             e.printStackTrace();
         }
     }
--- app/src/main/java/de/smarthome/app/repository/responsereactor/ResponseReactorGiraCallbackServer.java
+++ app/src/main/java/de/smarthome/app/repository/responsereactor/ResponseReactorGiraCallbackServer.java
@@ -5,15 +5,18 @@ import android.util.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import de.smarthome.app.repository.Repository;
 import de.smarthome.command.ResponseReactor;
 import de.smarthome.app.utility.ToastUtility;
 
 public class ResponseReactorGiraCallbackServer implements ResponseReactor {
     private static final String TAG = "ResponseReactorGiraCallbackServer";
     private ToastUtility toastUtility;
+    private Repository repository;
 
     public ResponseReactorGiraCallbackServer() {
-        this.toastUtility = ToastUtility.getInstance();
+        repository = Repository.getInstance();
+        toastUtility = ToastUtility.getInstance();
     }
 
     @Override
@@ -22,16 +25,18 @@ public class ResponseReactorGiraCallbackServer implements ResponseReactor {
             if (responseEntity.getStatusCode() == HttpStatus.OK) {
                 Log.d(TAG, "response received " + TAG);
                 Log.d(TAG, "No Problems when registering CallbackServer at Gira.\nStatus: " + responseEntity.getStatusCode());
+                repository.serverConnectionEvent(ServerConnectionEvent.GIRA_CONNECTION_SUCCESS);
             } else {
                 Log.d(TAG, "error occurred");
                 Log.d(TAG, responseEntity.getStatusCode().toString());
                 Log.d(TAG, "Problem when registering CallbackServer at Gira.\nStatus: " + responseEntity.getStatusCode());
-
                 toastUtility.prepareToast("Unable to reach Gira Server");
+                repository.serverConnectionEvent(ServerConnectionEvent.GIRA_CONNECTION_FAIL);
             }
         }catch(Exception e){
             Log.d(TAG, "Exception: " + e.toString());
             toastUtility.prepareToast("Exception: Unable to register CallbackServer at Gira!");
+            repository.serverConnectionEvent(ServerConnectionEvent.GIRA_CONNECTION_FAIL);
             e.printStackTrace();
         }
     }
