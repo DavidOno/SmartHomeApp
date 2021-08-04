4b9508f71f7ed614c4ef6c7502a63809066ecf80
Muriskk <skryreSlaveMail@gmail.com>
2021-08-04 16:27:29 +0200

    Added enum for status of connection to servers

---
+++ app/src/main/java/de/smarthome/app/repository/responsereactor/ServerConnectionEvent.java
@@ -0,0 +1,6 @@
+package de.smarthome.app.repository.responsereactor;
+
+public enum ServerConnectionEvent {
+    CALLBACK_CONNECTION_SUCCESS, CALLBACK_CONNECTION_FAIL, CALLBACK_CONNECTION_ACTIVE,
+    GIRA_CONNECTION_SUCCESS, GIRA_CONNECTION_FAIL, GIRA_CONNECTION_ACTIVE;
+}
