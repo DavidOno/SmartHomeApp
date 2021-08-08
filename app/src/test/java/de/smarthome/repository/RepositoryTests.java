5831cf4e5189b62408254ae009194302b4d5f079
Muriskk <skryreSlaveMail@gmail.com>
2021-08-09 01:03:17 +0200

    Added unit tests

--- app/src/test/java/de/smarthome/repository/RepositoryTests.java
+++ app/src/test/java/de/smarthome/repository/RepositoryTests.java
@@ -1,30 +1,43 @@
 package de.smarthome.repository;
 
-import android.app.Application;
-import android.content.Context;
-
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.MutableLiveData;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.google.android.gms.auth.api.credentials.Credential;
 
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
+import de.smarthome.app.model.Datapoint;
 import de.smarthome.app.model.Function;
 import de.smarthome.app.model.Location;
 import de.smarthome.app.model.UIConfig;
+import de.smarthome.app.model.configs.Channel;
+import de.smarthome.app.model.configs.ChannelConfig;
+import de.smarthome.app.model.configs.ChannelDatapoint;
+import de.smarthome.app.model.responses.CallBackServiceInput;
+import de.smarthome.app.model.responses.CallbackValueInput;
+import de.smarthome.app.model.responses.ServiceEvent;
 import de.smarthome.app.repository.Repository;
+import de.smarthome.app.repository.ServerCommunicator;
+import de.smarthome.app.repository.StatusRequestType;
+import de.smarthome.app.repository.responsereactor.ServerConnectionEvent;
+import de.smarthome.server.SmartHomeFMS;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RepositoryTests {
@@ -32,30 +45,36 @@ public class RepositoryTests {
     @Rule
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
 
-    private void fillWithDummyValueBeaconConfig() {
-        String locationConfigString = "{\n" +
-                "\t\"locations\": [\n" +
-                "\t\t{\n" +
-                "\t\t\t\"roomUID\": \"aafb\",\n" +
-                "\t\t\t\"beaconId\": \"7b44b47b-52a1-5381-90c2-f09b6838c5d420\"\n" +
-                "\t\t}]}";
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            //Repository.getInstance(null).setBeaconLocations(mapper.readValue(locationConfigString, new TypeReference<BeaconLocations>() {}));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    @Mock
+    ServerCommunicator mockSc;
+
+    @InjectMocks
+    private Repository r = Repository.getInstance();
 
     @After
     public void cleanUp(){
         Repository.destroyInstance();
     }
 
+    //ok?
+    @Test
+    public void restartConnectionToServer(){
+        MutableLiveData<Boolean> x = new MutableLiveData<>(true);
+        Mockito.when(mockSc.getServerConnectionStatus()).thenReturn(x);
+        Mockito.when(mockSc.getGiraServerConnectionStatus()).thenReturn(ServerConnectionEvent.GIRA_CONNECTION_FAIL);
+        Mockito.when(mockSc.getCallbackServerConnectionStatus()).thenReturn(ServerConnectionEvent.CALLBACK_CONNECTION_FAIL);
+
+        r.restartConnectionToServer();
+
+        verify(mockSc,  times(1)).retryConnectionToServer();
+        assertThat(r.getServerConnectionStatus().getValue()).isTrue();
+        assertThat(mockSc.getGiraServerConnectionStatus()).isEqualTo(ServerConnectionEvent.GIRA_CONNECTION_FAIL);
+        assertThat(mockSc.getCallbackServerConnectionStatus()).isEqualTo(ServerConnectionEvent.CALLBACK_CONNECTION_FAIL);
+    }
+
     //ok
     @Test
     public void testConfirmBeaconLocation(){
-        Repository r = Repository.getInstance();
         LinkedList<String> fl = new LinkedList<>();
         LinkedList<Location> ll = new LinkedList<>();
         Location location = new Location("l1", "1","dummy", fl, ll,"dummy");
@@ -67,69 +86,168 @@ public class RepositoryTests {
         assertThat(r.getBeaconLocation()).isNull();
     }
 
-    //assert BeaconCheck = false
-    //how to check that I get subscribed??
-
+    //ok
+     @Test
     public void initBeaconObserver(){
-        Application a = mock(Application.class);
-        Context c = mock(Context.class);
-        Mockito.when(a.getApplicationContext()).thenReturn(c);
-
-        a.getApplicationContext();
-        Repository r = Repository.getInstance();
-        r.setParentApplication(a);
-        //fillWithDummyValueBeaconConfig();
-        LinkedList<Function> emptyFunctionList = new LinkedList<>();
-        LinkedList<Location> emptyLocationList = new LinkedList<>();
-        UIConfig uiConfig = new UIConfig(emptyFunctionList, emptyLocationList, "1");
-        r.initNewUIConfig(uiConfig);
-
         r.initBeaconObserver();
 
         assertThat(r.getBeaconCheck().getValue()).isFalse();
     }
 
     //assert that notify MyFirebaseSubscriber ==> trigger update in Repo
-
+    @Test
     public void requestRegisterUser(){
-        Repository r = Repository.getInstance();
+        Credential mockCredential = mock(Credential.class);
+        Mockito.when(mockCredential.getId()).thenReturn("ID");
+        Mockito.when(mockCredential.getPassword()).thenReturn("password");
 
-        /*Credential credential = new Credential.Builder("userName")
-                .setPassword("pwd")
-                .build();
-        r.requestRegisterUser(credential);*/
+        r.requestRegisterUser(mockCredential);
 
-        //SmartHomeFMS.getServiceObserver().notify(new CallbackValueInput(0,"", "", null, null));
-        //SmartHomeFMS.getValueObserver().notify(new CallbackValueInput(0,"", "", null, null));
+        String testId = "testID";
+        String value = "199";
+
+        SmartHomeFMS.getServiceObserver().notify(new CallbackValueInput(0,"", "", null, "startup"));
+        SmartHomeFMS.getValueObserver().notify(new CallbackValueInput(0,"", testId, value, null));
+
+        assertThat(r.getStatusUpdateMap().getValue()).containsEntry(testId, value);
+        verify(mockSc,  times(1)).getSavedCredentialsForLoginAtGira();
+    }
+    @Test
+    public void requestCurrentStatusValuesWithEmptyLists(){
+        r.requestCurrentStatusValues(StatusRequestType.FUNCTION);
+        verify(mockSc,  times(0)).getSavedCredentialsForLoginAtGira();
+
+        r.requestCurrentStatusValues(StatusRequestType.DATAPOINT);
+        verify(mockSc,  times(0)).getSavedCredentialsForLoginAtGira();
     }
 
-    //assert length of requestlist
-    public void getFunctionMap(){
-        //because requestCurrentFunctionValues is called
-    }
-    //assert length of requestlist
-    public void getDataPointMap(){
-        //because requestCurrentDataPointValues is called
-    }
+    @Test
+    //Have to set a selected Location for Function map +
+    //Brauchen noch eine ChannelConfig mit Switch and StatusViewHolder
+    public void requestCurrentStatusValuesFunction(){
+        LinkedList<String> functionIdList = new LinkedList<>();
+        functionIdList.add("1");
+        functionIdList.add("2");
+        functionIdList.add("3");
+        functionIdList.add("4");
+        LinkedList<Location> emptyLocationList = new LinkedList<>();
+        Location location = new Location("l1", "1","dummy", functionIdList, emptyLocationList,"dummy");
 
-    //assert that cc.initStatusUpdateMap() is called
-    public void updateCallbackValueInput(){
-        //For CallbackValueInput
-    }
+        LinkedList<Datapoint> datapointList = new LinkedList<>();
+        datapointList.add(new Datapoint("1", "datapoint1"));
+        Function function = new Function("f1_function", "1", "de.gira.schema.channels.Switch", "dummy", datapointList);
+        LinkedList<Datapoint> datapointList2 = new LinkedList<>();
+        datapointList2.add(new Datapoint("2", "datapoint2"));
+        Function functionStatus = new Function("f1_Status", "2", "de.gira.schema.channels.Switch", "dummy", datapointList2);
 
-    //assert that  serverCommunicator.getSavedCredentialsForLoginAfterRestart() is called
-    //assert that  serverCommunicator.requestUIConfigAfterRestart() is called
-    //assert that  serverCommunicator.requestAdditionalConfigsAfterRestart() is called
-    //assert that  serverCommunicator.getSavedCredentialsForLoginAfterRestart() is called
-    //assert taht I reached RESTART && DEFAULT
-    public void updateCallBackServiceInput(){
-        //For CallBackServiceInput
+        LinkedList<Datapoint> datapointList3 = new LinkedList<>();
+        datapointList3.add(new Datapoint("3", "datapoint3"));
+        Function function2 = new Function("f2_function", "3", "de.gira.schema.channels.RoomTemperatureSwitchable", "dummy", datapointList3);
+        LinkedList<Datapoint> datapointList4 = new LinkedList<>();
+        datapointList4.add(new Datapoint("4", "datapoint4"));
+        Function functionStatus2 = new Function("f2_Status", "4", "de.gira.schema.channels.RoomTemperatureSwitchable", "dummy", datapointList4);
+
+        LinkedList<Function> functionList = new LinkedList<>();
+        functionList.add(function);
+        functionList.add(functionStatus);
+        functionList.add(function2);
+        functionList.add(functionStatus2);
+
+        LinkedList<Location> ll2 = new LinkedList<>();
+        ll2.add(location);
+        UIConfig uiConfig = new UIConfig(functionList, ll2, "1");
+        r.initNewUIConfig(uiConfig);
+        r.setChannelConfig(createDummyChannelConfig());
+        r.initSelectedLocation(location);
+
+        r.requestCurrentStatusValues(StatusRequestType.FUNCTION);
+
+        List<String> resultList = new ArrayList<>();
+        resultList.add("2");
+        resultList.add("4");
+        verify(mockSc,  times(1)).requestGetValue(resultList);
     }
 
     //ok
     @Test
-    public void testUpdateLocationWithNewLocation(){
-        Repository r = Repository.getInstance();
+    public void requestCurrentStatusValuesDataPoint(){
+        LinkedList<String> functionIdList = new LinkedList<>();
+        functionIdList.add("1");
+        functionIdList.add("2");
+        LinkedList<Location> emptyLocationList = new LinkedList<>();
+        Location location = new Location("l1", "1","dummy", functionIdList, emptyLocationList,"dummy");
+
+        LinkedList<Datapoint> datapointList = new LinkedList<>();
+        datapointList.add(new Datapoint("1", "datapoint1"));
+        Function function = new Function("f_function", "1", "dummy", "dummy", datapointList);
+        LinkedList<Datapoint> datapointList2 = new LinkedList<>();
+        datapointList2.add(new Datapoint("2", "datapoint2"));
+        Function functionStatus = new Function("f_Status", "2", "dummy", "dummy", datapointList2);
+
+        LinkedList<Function> functionList = new LinkedList<>();
+        functionList.add(function);
+        functionList.add(functionStatus);
+
+        LinkedList<Location> ll2 = new LinkedList<>();
+        ll2.add(location);
+        UIConfig uiConfig = new UIConfig(functionList, ll2, "2");
+        r.initNewUIConfig(uiConfig);
+        r.initSelectedLocation(location);
+        r.initSelectedFunction(function);
+
+        r.requestCurrentStatusValues(StatusRequestType.DATAPOINT);
+        List<String> resultList = new ArrayList<>();
+        resultList.add("2");
+        verify(mockSc,  times(1)).requestGetValue(resultList);
+    }
+
+    //ok
+    @Test
+    public void updateCallbackValueInputWithValue(){
+        String testId = "testID";
+        String value = "199";
+
+        CallbackValueInput callbackValueInput = new CallbackValueInput(0, "token", testId, value, null);
+        r.update(callbackValueInput);
+
+        assertThat(r.getStatusUpdateMap().getValue()).containsEntry(testId, value);
+    }
+
+    //ok
+    @Test
+    public void updateCallbackValueInputWithEvent(){
+        CallbackValueInput callbackValueInput = new CallbackValueInput(0, "token", null, null, "STARTUP");
+        r.update(callbackValueInput);
+        verify(mockSc,  times(1)).getSavedCredentialsForLoginAtGira();
+
+        callbackValueInput = new CallbackValueInput(0, "token", null, null, "UICONFIGCHANGED");
+        r.update(callbackValueInput);
+        verify(mockSc,  times(1)).requestOnlyUIConfig();
+
+        callbackValueInput = new CallbackValueInput(0, "token", null, null, "PROJECTCONFIGCHANGED");
+        r.update(callbackValueInput);
+        verify(mockSc,  times(1)).requestOnlyAdditionalConfigs();
+    }
+
+
+    //ok
+    @Test
+    public void updateCallBackServiceInput(){
+        List<ServiceEvent> eventList = new ArrayList<>();
+        eventList.add(new ServiceEvent("startup"));
+        eventList.add(new ServiceEvent("uiConfigChanged"));
+        eventList.add(new ServiceEvent("projectConfigChanged"));
+        CallBackServiceInput callBackServiceInput = new CallBackServiceInput(0, "token",  eventList);
+
+        r.update(callBackServiceInput);
+        verify(mockSc,  times(1)).getSavedCredentialsForLoginAtGira();
+        verify(mockSc,  times(1)).requestOnlyUIConfig();
+        verify(mockSc,  times(1)).requestOnlyAdditionalConfigs();
+    }
+
+    //ok
+    @Test
+    public void testUpdateLocationWithDifferentLocation(){
         LinkedList<String> emptyFunctionIdList = new LinkedList<>();
         LinkedList<Location> emptyLocationList = new LinkedList<>();
         Location location = new Location("l1", "1","dummy", emptyFunctionIdList, emptyLocationList,"dummy");
@@ -143,7 +261,6 @@ public class RepositoryTests {
     //ok
     @Test
     public void testUpdateLocationWithSameLocation(){
-        Repository r = Repository.getInstance();
         LinkedList<String> emptyFunctionIdList = new LinkedList<>();
         LinkedList<Location> emptyLocationList = new LinkedList<>();
         Location location = new Location("l1", "1","dummy", emptyFunctionIdList, emptyLocationList,"dummy");
@@ -157,6 +274,21 @@ public class RepositoryTests {
         assertThat(r.getBeaconCheck().getValue()).isFalse();
     }
 
-    //assert that all subscribers are empty
-    public void unsubscribeFromEverything(){}
+    private ChannelConfig createDummyChannelConfig(){
+        List<Channel> channelList = new ArrayList<>();
+        List<ChannelDatapoint> channelDatapoints = new ArrayList<>();
+
+        channelDatapoints.add(new ChannelDatapoint("OnOff", "Binary", "rwe"));
+        Channel c1 = new Channel("de.gira.schema.channels.Switch", channelDatapoints);
+        channelList.add(c1);
+
+        List<ChannelDatapoint> channelDatapoints2 = new ArrayList<>();
+        channelDatapoints2.add(new ChannelDatapoint("Current", "Float", "rwe"));
+        channelDatapoints2.add(new ChannelDatapoint("Set-Point", "Float", "rwe"));
+        channelDatapoints2.add(new ChannelDatapoint("OnOff", "Binary", "rwe"));
+        Channel c2 = new Channel("de.gira.schema.channels.RoomTemperatureSwitchable", channelDatapoints2);
+        channelList.add(c2);
+
+        return new ChannelConfig(channelList);
+    }
 }
--- app/src/test/java/de/smarthome/repository/ServerCommunicatorTests.java
+++ app/src/test/java/de/smarthome/repository/ServerCommunicatorTests.java
@@ -1,60 +1,228 @@
 package de.smarthome.repository;
 
-import android.app.Application;
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
-
-import com.google.android.gms.auth.api.credentials.Credential;
-
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import de.smarthome.app.model.UIConfig;
-import de.smarthome.app.model.responses.AvailabilityResponse;
-import de.smarthome.app.model.responses.RegisterResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.smarthome.SmartHomeApplication;
+import de.smarthome.app.model.responses.GetValueResponse;
+import de.smarthome.app.model.responses.UID_Value;
+import de.smarthome.app.repository.Repository;
 import de.smarthome.app.repository.ServerCommunicator;
+import de.smarthome.app.repository.responsereactor.ServerConnectionEvent;
+import de.smarthome.command.Command;
+import de.smarthome.command.CommandChain;
+import de.smarthome.command.MultiReactorCommandChain;
+import de.smarthome.command.impl.ChangeValueCommand;
+import de.smarthome.command.impl.GetValueCommand;
+import de.smarthome.command.impl.SingleReactorCommandChainImpl;
+import de.smarthome.command.impl.UnRegisterCallbackServerAtGiraServer;
+import de.smarthome.command.impl.UnregisterClientCommand;
 import de.smarthome.server.gira.GiraServerHandler;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 
-//@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class ServerCommunicatorTests {
 
-    //assert that RRCheckAvailability
-    //assert that RRClient
-    //assert that RRGiraCallBackServer
-    //assert that RRCallbackServer
-    //assert that RR for UI && Channel && Beacon && Boundary
-    // are called
-    public void initialisationOfApplication(){
-        GiraServerHandler gh = mock(GiraServerHandler.class);
-        ServerCommunicator sc = new ServerCommunicator(gh);
-        Credential credential = new Credential.Builder("userName")
-                .setPassword("pwd")
-                .build();
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
 
-        //sc.initialisationOfApplication(credential);
+    //ok
+    @Test
+    public void serverConnectionEventCallbackFail(){
+        ServerConnectionEvent event = ServerConnectionEvent.CALLBACK_CONNECTION_FAIL;
+        ServerCommunicator sc = new ServerCommunicator(null);
+
+        sc.serverConnectionEvent(event);
+
+        assertThat(sc.getCallbackServerConnectionStatus()).isEqualTo(event);
     }
 
-    //assert that RR UI
-    public void requestUIConfigAfterRestart(){}
+    //ok
+    @Test
+    public void serverConnectionEventCallbackSuccess(){
+        ServerConnectionEvent event = ServerConnectionEvent.CALLBACK_CONNECTION_SUCCESS;
+        ServerCommunicator sc = new ServerCommunicator(null);
 
-    //assert that RR Channel && Beacon && Boundary
-    public void requestAdditionalConfigsAfterRestart(){}
+        sc.serverConnectionEvent(event);
 
-    //assert that ChangeValueCommand
-    public void requestSetValue(){}
+        assertThat(sc.getCallbackServerConnectionStatus()).isEqualTo(event);
+    }
 
-    //assert that sc.handleResponseGetValue()
-    public void requestGetValue(){}
+    //ok
+    @Test
+    public void serverConnectionEventGiraFail(){
+        ServerConnectionEvent event = ServerConnectionEvent.GIRA_CONNECTION_FAIL;
+        ServerCommunicator sc = new ServerCommunicator(null);
 
-    //        requestUnregisterClient(IP_OF_CALLBACK_SERVER);
-    //        requestUnregisterCallbackServerAtGiraServer();
-    public void unsubscribeFromEverything(){}
+        sc.serverConnectionEvent(event);
 
-    //sc.initialisationOfApplicationAfterRestart()
-    //toast
-    public void getSavedCredentialsForLoginAfterRestart(){}
+        assertThat(sc.getGiraServerConnectionStatus()).isEqualTo(event);
+    }
+
+    //ok
+    @Test
+    public void serverConnectionEventGiraSuccess(){
+        ServerConnectionEvent event = ServerConnectionEvent.GIRA_CONNECTION_SUCCESS;
+        ServerCommunicator sc = new ServerCommunicator(null);
+
+        sc.serverConnectionEvent(event);
+
+        assertThat(sc.getGiraServerConnectionStatus()).isEqualTo(event);
+    }
+
+    //ok
+    @Test
+    public void retryConnectionToServerCallbackFail(){
+        ServerConnectionEvent event = ServerConnectionEvent.CALLBACK_CONNECTION_FAIL;
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+        sc.setCallbackServerConnectionStatus(event);
+
+        sc.retryConnectionToServer();
+
+        assertThat(sc.getCallbackServerConnectionStatus()).isEqualTo(ServerConnectionEvent.CALLBACK_CONNECTION_ACTIVE);
+    }
+
+    //ok
+    @Test
+    public void retryConnectionToServerGiraFail(){
+        ServerConnectionEvent event = ServerConnectionEvent.GIRA_CONNECTION_FAIL;
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+        sc.setGiraServerConnectionStatus(event);
+
+        sc.retryConnectionToServer();
+
+        assertThat(sc.getGiraServerConnectionStatus()).isEqualTo(ServerConnectionEvent.GIRA_CONNECTION_FAIL);
+    }
+
+    //ok
+    @Test
+    public void connectToGira(){
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+
+        sc.connectToGira("user", "pwd");
+
+        assertThat(sc.getGiraServerConnectionStatus()).isEqualTo(ServerConnectionEvent.GIRA_CONNECTION_ACTIVE);
+    }
+
+    //ok
+    @Test
+    public void connectToCallbackServer(){
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+
+        sc.connectToCallbackServer();
+
+        assertThat(sc.getCallbackServerConnectionStatus()).isEqualTo(ServerConnectionEvent.CALLBACK_CONNECTION_ACTIVE);
+    }
+
+    //ok
+    @Test
+    public void requestOnlyUIConfig(){
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+
+        sc.requestOnlyUIConfig();
+
+        ArgumentCaptor<CommandChain> argument = ArgumentCaptor.forClass(SingleReactorCommandChainImpl.class);
+        verify(mockGsh, times(1)).sendRequest(argument.capture());
+    }
+
+    //ok
+    @Test
+    public void requestOnlyAdditionalConfigs(){
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+
+        sc.requestOnlyAdditionalConfigs();
+
+        ArgumentCaptor<CommandChain> argument = ArgumentCaptor.forClass(MultiReactorCommandChain.class);
+        verify(mockGsh, times(1)).sendRequest(argument.capture());
+    }
+
+    //not OK
+    @Test
+    public void requestSetValue(){
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+        String testID = "TestID";
+        String value = "199";
+
+        Command setValueCommand = new ChangeValueCommand(testID, Float.parseFloat(value));
+        ResponseEntity<Command> myEntity = new ResponseEntity<>(setValueCommand, HttpStatus.OK);
+        Mockito.when(mockGsh.sendRequest(ArgumentMatchers.any(ChangeValueCommand.class))).thenReturn(myEntity);
+
+        sc.requestSetValue(testID, value);
+        sleep();
+
+        ArgumentCaptor<ChangeValueCommand> argument = ArgumentCaptor.forClass(ChangeValueCommand.class);
+        verify(mockGsh, times(1)).sendRequest(argument.capture());
+    }
+
+    //not ok
+    @Test
+    public void requestGetValue(){
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+        String testID = "TestID";
+        String value = "199";
+
+        List<UID_Value> values = new ArrayList<>();
+        values.add(new UID_Value(testID, value));
+        ResponseEntity<GetValueResponse> myEntity = new ResponseEntity<>(new GetValueResponse(values), HttpStatus.OK);
+        Mockito.when(mockGsh.sendRequest(ArgumentMatchers.any(GetValueCommand.class))).thenReturn(myEntity);
+
+        List<String> requestList = new ArrayList<>();
+        requestList.add(testID);
+
+        sc.requestGetValue(requestList);
+        sleep();
+
+        ArgumentCaptor<Command> argument = ArgumentCaptor.forClass(GetValueCommand.class);
+        verify(mockGsh, times(1)).sendRequest(argument.capture());
+
+        assertThat(Repository.getInstance().getStatusGetValueMap().getValue()).containsEntry(testID, value);
+    }
+
+    //not ok
+    @Test
+    public void unsubscribeFromEverything(){
+        GiraServerHandler mockGsh = mock(GiraServerHandler.class);
+        ServerCommunicator sc = new ServerCommunicator(mockGsh);
+
+        sc.unsubscribeFromEverything();
+        sleep();
+
+        ArgumentCaptor<Command> argument1 = ArgumentCaptor.forClass(UnregisterClientCommand.class);
+        ArgumentCaptor<Command> argument2 = ArgumentCaptor.forClass(UnRegisterCallbackServerAtGiraServer.class);
+        verify(mockGsh, times(1)).sendRequest(argument1.capture());
+        verify(mockGsh, times(1)).sendRequest(argument2.capture());
+    }
+
+    private void sleep(){
+        try{
+            Thread.sleep(10);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
